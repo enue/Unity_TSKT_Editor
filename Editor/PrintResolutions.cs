ae0ecb194e4ad7286b599c34e4336929c8434a1a
enue <nnp@car.ocn.ne.jp>
2019-11-06 15:24:45 +0900

    0.0.1 PrintResolutions

---
+++ Editor/PrintResolutions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+namespace TSKT
+{
+    public class PrintResolutions
+    {
+        [MenuItem("TSKT/Print Resolutions")]
+        static void Print()
+        {
+            foreach (var it in Screen.resolutions)
+            {
+                Debug.Log(new Vector2Int(it.width, it.height));
+            }
+        }
+    }
+}
