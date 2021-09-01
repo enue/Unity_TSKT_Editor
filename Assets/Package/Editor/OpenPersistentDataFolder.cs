551710c9df4931b02e2fd55ae8b45b4888b92605
enu <otoriden@gmail.com>
2021-09-01 20:51:56 +0900

    OpenPersistentDataFolder

---
+++ Assets/Package/Editor/OpenPersistentDataFolder.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Linq;
+
+namespace TSKT
+{
+    public class OpenPersistentDataFolder
+    {
+        [MenuItem("TSKT/Open PersistentDataFolder")]
+        static void OpenP()
+        {
+            Application.OpenURL(Application.persistentDataPath);
+        }
+    }
+}
