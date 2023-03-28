c76c07fe4ce6249e215097351a08de3e02d397ae
enue <otoriden@gmail.com>
2023-03-28 09:31:21 +0900

    capture SceneView

--- Assets/Package/Editor/CaptureMenuItem.cs
+++ Assets/Package/Editor/CaptureMenuItem.cs
@@ -7,19 +7,48 @@ namespace TSKT
 {
     public class CaptureMenuItem
     {
-        [MenuItem("TSKT/Capture")]
+        [MenuItem("TSKT/Capture GameView")]
         static void Save()
+        {
+            var path = GeneratePath(out var dir);
+
+            ScreenCapture.CaptureScreenshot(path);
+
+            Application.OpenURL(dir);
+            Debug.Log($"saved {path}");
+        }
+
+        [MenuItem("TSKT/Capture SceneView")]
+        static void CaptureSceneView()
+        {
+            var camera = SceneView.lastActiveSceneView.camera;
+            var renderTexture = camera.targetTexture;
+            var rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            var screenShot = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
+
+            camera.Render();
+            var original = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            screenShot.ReadPixels(rect, 0, 0);
+            RenderTexture.active = original;
+
+            var bytes = screenShot.EncodeToPNG();
+            var path = GeneratePath(out var dir);
+            File.WriteAllBytes(path, bytes);
+
+            Application.OpenURL(dir);
+            Debug.Log($"saved {path}");
+        }
+
+        static string GeneratePath(out string directory)
         {
             var dir = Path.Combine(Application.persistentDataPath, "Capture");
             Directory.CreateDirectory(dir);
 
             var now = System.DateTime.Now;
             var path = Path.Combine(dir, now.ToString("yyyyMMddHHmmss") + now.Millisecond.ToString("D3") + ".png");
-
-            ScreenCapture.CaptureScreenshot(path);
-
-            Application.OpenURL(dir);
-            Debug.Log($"screenshot saved at {path}");
+            directory = dir;
+            return path;
         }
     }
 }
